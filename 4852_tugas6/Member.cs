fb47be9034257ad7a07892b6b66b52b5e50497ed
winda <windaardelia@students.amikom.ac.id>
2023-04-13 00:03:28 +0700

    progam enkapsulasi

---
+++ 4852_tugas6/Member.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4852_tugas6
+{
+    class Member
+    {
+        private string name;
+        private int line;
+
+        public Member(string n, int l)
+        {
+            name = n;
+            line = l;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Line
+        {
+            get { return line; }
+            set { line = value; }
+        }
+
+
+    }
+}
---
+++ 4852_tugas6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4852_tugas6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Member m = new Member("Jae", 92);
+
+            Console.WriteLine("======================================");
+            Console.WriteLine("Name " + m.Name + ", Line " + m.Line);
+
+            Console.WriteLine("");
+
+            m.Name = "Brian";
+            m.Line = 93;
+
+            Console.WriteLine("Name " + m.Name + ", Line " + m.Line);
+            Console.WriteLine("======================================");
+
+            Console.WriteLine("");
+        }
+    }
+}
